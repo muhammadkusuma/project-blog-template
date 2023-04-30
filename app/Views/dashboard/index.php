afda2096b9d55ee4e32102d078b18fdc35864581
muhammadkusuma <mwiraadekusuma1@gmail.com>
2023-04-30 18:44:02 +0700

    update dashbor konten

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -34,69 +34,66 @@
         <!-- Main content -->
         <section class="content">
 
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <a href="<?php echo base_url('post/create'); ?>" class="btn btn-success btn-sm float-right">Konten Baru</a>
-                        </div>
-                        <div class="card-body">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <a href="<?php echo base_url('post/create'); ?>" class="btn btn-success btn-sm float-right">Konten Baru</a>
+                    </div>
+                    <div class="card-body">
 
-                            <?php if (session()->getFlashdata('success')) { ?>
-                                <div class="alert alert-success">
-                                    <?php echo session()->getFlashdata('success'); ?>
-                                </div>
-                            <?php } ?>
+                        <?php if (session()->getFlashdata('success')) { ?>
+                            <div class="alert alert-success">
+                                <?php echo session()->getFlashdata('success'); ?>
+                            </div>
+                        <?php } ?>
 
-                            <?php if (session()->getFlashdata('error')) { ?>
-                                <div class="alert alert-danger">
-                                    <?php echo session()->getFlashdata('error'); ?>
-                                </div>
-                            <?php } ?>
+                        <?php if (session()->getFlashdata('error')) { ?>
+                            <div class="alert alert-danger">
+                                <?php echo session()->getFlashdata('error'); ?>
+                            </div>
+                        <?php } ?>
 
-                            <table class="table table-bordered">
-                                <thead class="text-center">
-                                    <tr>
-                                        <th scope="col">Judul</th>
-                                        <th scope="col">Slug</th>
-                                        <th scope="col">Terbit</th>
-                                        <th scope="col">Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody class="text-center">
-                                    <?php if (!empty($posts) && is_array($posts)) { ?>
-                                        <?php foreach ($posts as $row) { ?>
-                                            <?php if ($row['id_menu'] === null) { ?>
-                                                <tr>
-                                                    <td><?php echo $row['judul']; ?></td>
-                                                    <td><?php echo $row['slug']; ?></td>
-                                                    <td><?php echo $row['created_at']; ?></td>
-                                                    <td>
-                                                        <a href="<?php echo base_url('post/edit/' . $row['id']); ?>" class="btn btn-primary btn-sm">Ubah</a>
-                                                        <a href="<?php echo base_url('post/destroy/' . $row['id']); ?>" class="btn btn-danger btn-sm" onclick="return confirm('Kamu yakin ingin menghapus <?= $row['judul']; ?> ?');">Hapus</a>
-                                                    </td>
-                                                </tr>
-                                            <?php } ?>
+                        <table class="table table-bordered">
+                            <thead class="text-center">
+                                <tr>
+                                    <th scope="col">Judul</th>
+                                    <th scope="col">Slug</th>
+                                    <th scope="col">Terbit</th>
+                                    <th scope="col">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                                <?php if (!empty($posts) && is_array($posts)) { ?>
+                                    <?php foreach ($posts as $row) { ?>
+                                        <?php if ($row['id_menu'] === null) { ?>
+                                            <tr>
+                                                <td><?php echo $row['judul']; ?></td>
+                                                <td><?php echo $row['slug']; ?></td>
+                                                <td><?php echo $row['created_at']; ?></td>
+                                                <td>
+                                                    <a href="<?php echo base_url('post/edit/' . $row['id']); ?>" class="btn btn-primary btn-sm">Ubah</a>
+                                                    <a href="<?php echo base_url('post/destroy/' . $row['id']); ?>" class="btn btn-danger btn-sm" onclick="return confirm('Kamu yakin ingin menghapus <?= $row['judul']; ?> ?');">Hapus</a>
+                                                </td>
+                                            </tr>
                                         <?php } ?>
-                                    <?php } else { ?>
-                                        <tr>
-                                            <td colspan="4" class="text-center">No post found.</td>
-                                        </tr>
                                     <?php } ?>
+                                <?php } else { ?>
+                                    <tr>
+                                        <td colspan="4" class="text-center">No post found.</td>
+                                    </tr>
+                                <?php } ?>
 
-                                </tbody>
-                            </table>
+                            </tbody>
+                        </table>
 
-                            <br>
+                        <br>
 
-                            <?= $pager->links(); ?>
-
-                        </div>
+                        <?= $pager->links(); ?>
 
                     </div>
+
                 </div>
             </div>
-
 
         </section>
         <!-- /.content -->
